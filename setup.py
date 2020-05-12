a1ea3fc74bccef86c432cab1b362adb419627b94
ariel ofer <bgzahav8@gmail.com>
2020-05-12 22:14:32 +0300

    __init__.py --> __version__

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ build your own gamebook easily"""
 
 setup(
     name="gamebook-maker",
-    version="0.0.1",
+    version="0.1.0",
     description="build your own gamebook with python",
     long_description=description,
     long_description_content_type="text",
