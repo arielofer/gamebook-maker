0f0e2398b26ab5170037dbccbef3c977648c85fc
ariel ofer <bgzahav8@gmail.com>
2020-05-12 22:58:31 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="0.1.0",
     description="build your own gamebook with python",
     long_description=description,
-    long_description_content_type="text",
+    long_description_content_type="text/markdown",
     url="https://github.com/arielofer/gamebook-maker",
     author="Ariel Ofer",
     packages=["gamebook"],
