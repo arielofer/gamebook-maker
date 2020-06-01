0f92ca367319145a7f4e21ef166f5142ac5ab254
ariel ofer <bgzahav8@gmail.com>
2020-06-01 15:43:39 +0300

    added a space as well

--- gamebook/terminal_input.py
+++ gamebook/terminal_input.py
@@ -14,7 +14,7 @@ class TerminalInput(Input):
         for option in options:
             user_input_string += (f"to {option.show_title()} enter one of the"
                                   f" following {option.show_user_inputs()}\n")
-        user_input_string += f"to exit the game enter:{exit_user_input}\n\n"
+        user_input_string += f"to exit the game enter: {exit_user_input}\n\n"
         user_input = input(user_input_string + "your choice:")
 
         return user_input
