2e3be4b437dfbe4ecde2d6ead6f8227b3b67ca95
ariel ofer <bgzahav8@gmail.com>
2020-06-01 15:38:07 +0300

    added custom exceptions + a file for important messages

---
+++ gamebook/exceptions.py
@@ -0,0 +1,13 @@
+class ReachedTheEndError(Exception):
+    """
+    the end of the current plot line has been reached.
+    the current scene has no 'options'
+    """
+
+
+class ExitRequested(Exception):
+    """the user sent a request to exit the game"""
+
+
+class OptionNotFoundError(Exception):
+    """no option with the recieved user_input found"""
---
+++ gamebook/important_strings.py
@@ -0,0 +1,19 @@
+"""
+a collection of all the messages and strings for different errors
+and conditions to use globally in the library
+
+feel free to edit them according to your preference
+"""
+
+
+# a message for when the player exits the game
+exit_message = "exiting..."
+
+# the user_input that the player enters to quit the game
+exit_user_input = "quit"
+
+# a message for when the player reaches the end of the plot line
+end_message = "you reached the end - game over"
+
+# a message that tells the player he entered a invalid user_input
+invalid_user_input_message = "this is an invalid choice. please try again"
