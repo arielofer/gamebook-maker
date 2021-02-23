1d175dd5e39a730111cb8cc26c575db39c36a68b
arielofer <bgzahav8@gmail.com>
2021-02-23 19:47:08 +0200

    added two new exceptions

--- gamebook/exceptions.py
+++ gamebook/exceptions.py
@@ -11,3 +11,14 @@ class ExitRequested(Exception):
 
 class OptionNotFoundError(Exception):
     """no option with the recieved user_input found"""
+
+
+class TraitNotFoundError(Exception):
+    """specified trait name couldn't be found in the trait list"""
+    def __init__(self, trait_name):
+        self.trait_name = trait_name
+
+
+class NextSceneTypeError(TypeError):
+    """the value of the next scene field in Scene isn't a NextScene object
+       or a scene name"""
