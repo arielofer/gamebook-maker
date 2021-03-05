f7f29ee017cca6881ba900217b7644062b161ee8
arielofer <bgzahav8@gmail.com>
2021-03-05 16:07:37 +0200

    implemented the strategy design pattern to the output class

---
+++ gamebook/output_interface.py
@@ -0,0 +1,15 @@
+class OutputInterface(object):
+    """
+    declares operations common to all supported kinds of output interfaces
+    the Output uses this class to call the fuctions defined in the different
+    output interfaces.
+    """
+
+    def output(self, content: str):
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
+
+    def exit(self, exit_reason: str):
+        raise NotImplementedError
