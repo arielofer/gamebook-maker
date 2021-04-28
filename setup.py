ad338a0acfb22ac21ad89de6714cfe0a6e720ad1
arielofer <bgzahav8@gmail.com>
2021-04-28 22:56:51 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
+import pathlib
 from setuptools import setup
 
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 description = """a library containing classes and functions so you could
 build your own gamebook easily"""
 
 setup(
     name="gamebook-maker",
-    version="0.1.0",
-    description="build your own gamebook with python",
-    long_description=description,
+    version="0.2.0",
+    description=description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/arielofer/gamebook-maker",
     author="Ariel Ofer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=["gamebook"],
 )
