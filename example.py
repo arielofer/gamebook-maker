b72b52d4d42a418f108b1eb42b5effc7c6f02468
arielofer <bgzahav8@gmail.com>
2021-05-26 22:39:35 +0300

    more developed example

--- example.py
+++ example.py
@@ -7,24 +7,6 @@ from gamebook.nextscene import NextScene
 from gamebook.game_manager import GameManager
 import gamebook.constants
 
-flee_scene = Scene(
-    name="fleeing scene",
-    desc='game over - you fled',
-    options=[]
-)
-
-lucky_scene = Scene(
-    name="luck scene",
-    desc='game over - you won',
-    options=[]
-)
-
-dead_scene = Scene(
-    name="death scene",
-    desc='game over - you died',
-    options=[]
-)
-
 monster_skill = Trait(name=gamebook.constants.power_trait_name,
                       min_value=1, amount=1)
 monster_stamina = Trait(name=gamebook.constants.health_trait_name,
@@ -32,47 +14,108 @@ monster_stamina = Trait(name=gamebook.constants.health_trait_name,
 
 imp = Monster(name="imp", traits=[monster_skill, monster_stamina])
 
-fight_scene = FightScene(
+trap_door_scene = Scene(
+    name="trap door",
+    desc='''you walk the path when suddenly the floor under you drops.
+you crash to to ground and the walls around you are greatly tall and slippery.
+you are stuck and unable to get out- game over''',
+    options=[]
+)
+
+failed_to_escape = Scene(
+    name="failed fleeing scene",
+    desc='''you failed to flee the imp. he managed to grab you from behind and kill you.
+game over - you died''',
+    options=[]
+)
+
+flee_scene = Scene(
+    name="fleeing scene",
+    desc='game over - you fled',
+    options=[]
+)
+
+fountain_scene = Scene(
+    name="fountain scene",
+    desc='''you feel your skin rejuvenated and more powerful then ever.
+your journey is now completed.
+game over - you won''',
+    options=[]
+)
+
+dead_scene = Scene(
+    name="death scene",
+    desc='the imp defeated you.\ngame over - you died',
+    options=[]
+)
+
+left_at_fork = Scene(
+    name="left at fork",
+    desc='''you keep walking the dark path, watching your steps.
+then suddenly you witness a little spot of light. you walk towards it and
+it grows as you get nearer it. it is an exit! you exit the maze and witness
+your sought after treasure - the fountain of youth...
+do you enter the water?''',
+    options=[
+        Option('enter the fountain', user_inputs=['enter'],
+               next_scenes="fountain scene")
+    ]
+)
+
+after_fight_scene = Scene(
+    name="after fight with imp",
+    desc='''congratulations, you defeated the imp.
+you stare at the imp's corpse then at the wall infront of you.
+you understand that you need to go back to the junction and take the other way''',
+    options=[
+        Option('go back and continue', user_inputs=['go back'],
+               next_scenes="left path")
+    ]
+)
+
+imp_fight_scene = FightScene(
     name="fight scene",
     desc="you fight the imp",
     next_scenes=[
-        NextScene(lucky_scene),
+        NextScene(after_fight_scene),
         NextScene(dead_scene)
     ],
     enemy=imp
 )
 
-next_scene2 = Scene(
-    name="scene2",
+right_at_intro = Scene(
+    name="right at intro",
     desc='''you find yourself in a dead end facing a blue imp.
 what do you do?''',
     options=[
         Option('Fight', user_inputs=['fight'],
                next_scenes=[
-            NextScene(lucky_scene, success_rate=0.5),
-            NextScene(dead_scene, success_rate=0.5)
+            NextScene(imp_fight_scene)
         ]),
         Option('Flee', user_inputs=['flee'],
                next_scenes=[
             NextScene(flee_scene, success_rate=0.5),
-            NextScene(dead_scene, success_rate=0.5)
+            NextScene(failed_to_escape, success_rate=0.5)
         ])
     ]
 )
 
-next_scene1 = Scene(
+left_at_intro = Scene(
     name="left path",
-    desc='''you are in a narrow corridor facing a fiery looking imp.
-do you fight or run away?''',
+    desc='''you are in a narrow corridor facing a three legged fork in the way.
+only the middle one seems to be dimmly lit. which way do you pick?''',
     options=[
-        Option('Fight', user_inputs=['fight'],
+        Option('left', user_inputs=['left'],
                next_scenes=[
-            NextScene(fight_scene)
+            NextScene(left_at_fork)
         ]),
-        Option('Flee', user_inputs=['flee'],
+        Option('middle', user_inputs=['middle'],
                next_scenes=[
-            NextScene(flee_scene, success_rate=0.5),
-            NextScene(dead_scene, success_rate=0.5)
+            NextScene(trap_door_scene)
+        ]),
+        Option('right', user_inputs=['right'],
+               next_scenes=[
+            NextScene(trap_door_scene)
         ])
     ]
 )
@@ -84,14 +127,15 @@ as you enter the maze. you walk the long straight path suddenly
 you face a junction. choose to left or right""",
     options=[
         Option('Go left', user_inputs=['l', 'left'],
-               next_scenes=NextScene(next_scene1)),
+               next_scenes=NextScene(left_at_intro)),
         Option('Go right', user_inputs=['r', 'right'],
-               next_scenes="scene2")
+               next_scenes="right at intro")
     ]
 )
 
-scenes_list = [intro_scene, next_scene1, next_scene2, fight_scene, dead_scene,
-               lucky_scene, flee_scene]
+scenes_list = [intro_scene, left_at_intro, right_at_intro, imp_fight_scene,
+               after_fight_scene, dead_scene, fountain_scene, flee_scene,
+               left_at_fork, failed_to_escape, trap_door_scene]
 
 
 def main():
